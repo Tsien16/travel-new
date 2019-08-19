168457bb89f7db47d78ce1529c50c75796e2276c
tsien16 <tsien16@qq.com>
2019-08-20 02:18:21 +0800

    init

--- src/views/city/City.vue
+++ src/views/city/City.vue
@@ -2,16 +2,49 @@
     <div>
         <city-header />
         <city-search />
+        <city-list :cities="cities" :hotCities="hotCities" :letter="letter" />
+        <city-alphabet :cities="cities" @change="handleLetterChange" />
     </div>
 </template>
 
 <script>
   import CityHeader from './components/CityHeader';
   import CitySearch from './components/CitySearch';
+  import CityList from './components/CityList';
+  import CityAlphabet from './components/CityAlphabet';
+  import axios from 'axios';
 
   export default {
     name: 'City',
-    components: {CitySearch, CityHeader},
+    components: {CityAlphabet, CityList, CitySearch, CityHeader},
+    data() {
+      return {
+        cities: {},
+        hotCities: [],
+        letter: '',
+      };
+    },
+    methods: {
+      getCityInfo() {
+        axios.get('/api/city.json').then(
+          this.handleGetCityInfoSuccess,
+        );
+      },
+      handleGetCityInfoSuccess(res) {
+        res = res.data;
+        if (res.ret && res.data) {
+          const data = res.data;
+          this.cities = data.cities;
+          this.hotCities = data.hotCities;
+        }
+      },
+      handleLetterChange(letter) {
+        this.letter = letter;
+      },
+    },
+    mounted() {
+      this.getCityInfo();
+    },
   };
 </script>
 
